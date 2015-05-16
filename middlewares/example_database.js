5c90aa830e81f7bb3fd6e6dd3c0d27bf4e936bbf
mrhodes <mrhodes262@gmail.com>
2015-05-16 10:42:58 -0500

    Updated gitignore and made example db connection file

---
+++ middlewares/example_database.js
@@ -0,0 +1,8 @@
+// config/database.js
+// Not for use in production - just here to show what you need.
+// Replace the localhost with your relevant connection info
+module.exports = {
+
+    'url' : 'mongodb://localhost/example_db'
+
+};
